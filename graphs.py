f727dafb914828ece58ad50e2bab32f35ef8f1b0
Troy Appel <troyappel@college.harvard.edu>
2022-02-24 21:19:00 -0500

    Add physical machine graph as undirected networkx graph

--- graphs.py
+++ graphs.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import math
 
 from enum import Enum
 
@@ -14,10 +15,25 @@ class NodeState(Enum):
     RUNNING = 2
     COMPLETED = 3
 
+def to_id(key):
+    if isinstance(key, ProgramNode):
+        return key.id
+    elif isinstance(key, str):
+        return key
+    elif isinstance(key, tuple):
+        return tuple(to_id(k) for k in key)
+    else:
+        return str(key)
+
 class ProgramNode(object):
+    """
+    Representation of Program Node.
+
+    Get this via a ProgramGraph method, or by iterating through it.
+    """
     def __init__(self, compute, memory, id=None, affinity=None):
         if id is None:
-            self.id = f"{gensym()}-{compute}-{memory}"
+            self.id = f"pn-{gensym()}-{compute}-{memory}"
         else:
             self.id = str(id)
 
@@ -32,19 +48,76 @@ class ProgramNode(object):
         else:
             self.affinity = affinity
 
+class ProgramEdge(object):
+    """
+    Program edge representation.
+
+    Stores IDs only of associated nodes, letting us define edges before the graph.
+    Get this by a ProgramGraph method.
+    """
+    def __init__(self, in_node, out_node, cost=0):
+        self.id = (to_id(in_node), to_id(out_node))
+        self.in_node = to_id(in_node)
+        self.out_node = to_id(out_node)
+        self.cost = cost
+
+############ MACHINES
+
+class MachineNode:
+    """
+    Representation of physical nodes.
+
+    Get this by iterating through/finding neighbors in a MachineGraph.
+    """
+    def __init__(self, compute, ram, id=None):
+        if id is None:
+            self.id = f"mn-{gensym()}-{compute}-{ram}"
+        else:
+            self.id = str(id)
+
+        self.compute = compute
+        self.ram = ram
+
+        self.task = None
+
+        self.stored_outputs = set()
+
+class MachineEdge:
+    """
+    Representation of edge between physical nodes. Stores ids of the machines, NOT machine nodes.
+
+    Get this by requesting an edge from a MachineGraph.
+    """
+    def __init__(self, m1, m2, latency=0, bandwidth=10):
+        self.id = to_id(m1), to_id(m2)
+        self.machine1 = to_id(m1)
+        self.machine2 = to_id(m2)
+        self.latency = latency
+        self.bandwidth = bandwidth
+
+
+##################
+### GRAPHS ###
+##################
+
 class ProgramGraph(object):
     """
     Representation of a program graph.
 
-    Thin wrapper around networkx.DiGraph. Lets you add/remove nodes, get neighbors, iterate,
-    and get more info by key. Keys can be a list of nodes, a node, or a node id.
+    Thin wrapper around networkx.DiGraph. Lets you add/remove nodes, get neighbors, iterate, etc.
+    All methods handle either a ProgramNode object or its ID.
     """
-    def __init__(self, nodes=None, edges=None):
-        self.node_dict = {pn.id: pn for pn in nodes}
+    def __init__(self, nodes=None, edges=None, strict=True):
+
         self.G = nx.DiGraph()
 
-        for pn in list(self.node_dict):
-            self.G.add_node(pn)
+        self.node_dict = {}
+        self.edge_dict = {}
+
+        self.strict = strict
+
+        if nodes is not None:
+            self.add_nodes(nodes)
 
         if edges is not None:
             self.add_edges(edges)
@@ -58,48 +131,61 @@ class ProgramGraph(object):
             pass
 
         # Check that there are no duplicate ids
-        assert(len(self.node_dict.keys()) == len(set(self.node_dict.keys())))
+        if len(self.node_dict.keys()) != len(set(self.node_dict.keys())):
+            return False
 
-    @staticmethod
-    def _get_id(key):
-        if isinstance(key, ProgramNode):
-            return key.id
-        elif isinstance(key, str):
-            return key
-        else:
-            return str(key)
+        return True
 
     def __getitem__(self, key):
         if isinstance(key, list):
-            return [self.node_dict[self._get_id(_key)] for _key in key]
+            return [self.node_dict[to_id(_key)] for _key in key]
         else:
-            return self.node_dict[self._get_id(key)]
+            return self.node_dict[to_id(key)]
 
     def pred(self, key):
-        return self.G.predecessors(self._get_id(key))
+        preds = self.G.predecessors(to_id(key))
+        return self[preds]
 
     def succ(self, key):
-        return self.G.successors(self._get_id(key))
+        succs = self.G.successors(to_id(key))
+        return self[succs]
+
+    def in_edges(self, key):
+        eds = self.G.in_edges(to_id(key))
+        return [self.edge_dict[ed] for ed in eds]
+
+    def out_edges(self, key):
+        eds = self.G.out_edges(to_id(key))
+        return [self.edge_dict[ed] for ed in eds]
 
     def add_node(self, pn):
         self.G.add_node(pn.id)
-        assert(pn.id not in self.node_dict)
+        if self.strict:
+            assert(pn.id not in self.node_dict)
         self.node_dict[pn.id] = pn
 
-    def add_edge(self, key1, key2):
-        id1 = self._get_id(key1)
-        id2 = self._get_id(key2)
+    def add_nodes(self, pn_list):
+        for pn in pn_list:
+            self.add_node(pn)
 
-        assert (id1 != id2)
+    def add_edge(self, en):
+
+        id1 = to_id(en.in_node)
+        id2 = to_id(en.out_node)
+
+        if self.strict:
+            assert (id1 != id2)
+            assert (id1 in self.node_dict)
+            assert (id2 in self.node_dict)
+            assert((id1, id2) not in self.node_dict)
+            assert((id1, id2) not in self.G.edges)
 
         self.G.add_edge(id1, id2)
+        self.edge_dict[(id1, id2)] = en
 
-    def add_edges(self, edge_list):
-        for (id1, id2) in edge_list:
-            self.add_edge(id1, id2)
-
-    def __iter__(self):
-        yield from self.node_dict.values()
+    def add_edges(self, en_list):
+        for en in en_list:
+            self.add_edge(en)
 
     def draw(self, blocking=True):
         colors = ["#AAAAAA", "#ACC8DC", "#D8315B", "#1E1B18"]
@@ -111,11 +197,134 @@ class ProgramGraph(object):
 
         nx.draw_networkx_labels(self.G, pos)
 
-        nx.draw_networkx_edges(self.G, pos, self.G.edges, arrows=False)
+        # Draw based on cost of edge
+        costs = [self.edge_dict[tup].cost for tup in self.G.edges]
+
+        nx.draw_networkx_edges(self.G, pos, self.G.edges, arrows=True, edge_color=costs, edge_cmap=plt.get_cmap('copper'))
 
         if blocking:
             plt.show()
         else:
             plt.pause(0.001)
 
+    def __iter__(self):
+        yield from self.node_dict.values()
 
+
+
+class MachineGraph:
+    """
+    Representation of physical graph.
+
+    Internally represented as an undirected networkx graph, in case we want graph algos.
+    Generally works
+    """
+    def __init__(self, nodes=None, edges=None, strict=True):
+        self.G = nx.Graph()
+
+        self.node_dict = {}
+        self.edge_dict = {}
+
+        self.strict = strict
+
+        if nodes is not None:
+            self.add_nodes(nodes)
+
+        if edges is not None:
+            self.add_edges(edges)
+
+    def validate(self):
+        # Check that there are no duplicate ids
+        if len(self.node_dict.keys()) != len(set(self.node_dict.keys())):
+            return False
+
+        return True
+
+    def neighbors(self, key):
+        neigh = self.G.neighbors(to_id(key))
+        return self[neigh]
+
+    def edges(self, key):
+        eds = self.G.edges(to_id(key))
+        return [self.edge_dict[ed] for ed in eds]
+
+    def add_node(self, pn):
+        self.G.add_node(pn.id)
+        if self.strict:
+            assert(pn.id not in self.node_dict)
+        self.node_dict[pn.id] = pn
+
+    def add_nodes(self, pn_list):
+        for pn in pn_list:
+            self.add_node(pn)
+
+    def add_edge(self, en):
+
+        id1 = to_id(en.machine1)
+        id2 = to_id(en.machine2)
+
+        if self.strict:
+            assert (id1 != id2)
+            assert (id1 in self.node_dict)
+            assert (id2 in self.node_dict)
+            assert((id1, id2) not in self.node_dict)
+            assert((id1, id2) not in self.G.edges)
+
+        self.G.add_edge(id1, id2)
+        self.edge_dict[(id1, id2)] = en
+
+    def add_edges(self, en_list):
+        for en in en_list:
+            self.add_edge(en)
+
+    def draw(self, blocking=True, linewidth_exp=0.5):
+        """
+        Draw this graph using matplotlib.
+
+        :param blocking: Whether to block the program while displaying the graph.
+        :param linewidth_exp: Exponent to determine line width based on bandwidth, between 0 and 1.
+        """
+        colors = ["#AAAAAA", "#ACC8DC", "#D8315B", "#1E1B18"]
+        taskless_color = "#7788AA"
+        pos = nx.spring_layout(self.G)
+
+        taskless = [n for n in self.G if self.node_dict[n].task is None]
+        tasked = [n for n in self.G if self.node_dict[n].task is not None]
+
+        for ns in NodeState:
+            nodes_ns = [n.task.state for n in tasked if self.node_dict[n].task.state == ns]
+            nx.draw_networkx_nodes(self.G, pos, nodelist=nodes_ns, node_size=500, node_color=colors[ns.value])
+
+        nx.draw_networkx_nodes(self.G, pos, nodelist=taskless, node_size=500, node_color=taskless_color)
+
+        nx.draw_networkx_labels(self.G, pos)
+
+        # Draw based on cost of edge
+        latencies = [self.edge_dict[tup].latency for tup in self.G.edges]
+        bandwidths = [1 + self.edge_dict[tup].bandwidth**linewidth_exp for tup in self.G.edges]
+
+        nx.draw_networkx_edges(self.G, pos, self.G.edges, width=bandwidths,  edge_color=latencies, edge_cmap=plt.get_cmap('copper'))
+
+        if blocking:
+            plt.show()
+        else:
+            plt.pause(0.001)
+
+    def __getitem__(self, key):
+        if isinstance(key, list):
+            return [self.node_dict[to_id(_key)] for _key in key]
+        if isinstance(key, tuple):
+            return self.edge_dict[to_id(key)]
+        else:
+            return self.node_dict[to_id(key)]
+
+    def __iter__(self):
+        yield from self.node_dict.values()
+
+
+# Example usage
+# nodes = [ProgramNode(1, 0, 1), ProgramNode(2, 0, 2), ProgramNode(3,0,3)]
+# edges = [ProgramEdge(1,2), ProgramEdge(1,3, cost=1), ProgramEdge(2,1)]
+# g = ProgramGraph(nodes, edges)
+# print(g.validate())
+# g.draw()
