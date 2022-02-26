d049e129b771b6492f3270ec39de38ccdfbe872a
Jordan Barkin <jordanbarkin@me.com>
2022-02-26 04:12:21 -0500

    a first pass at the simulator routine

--- events.py
+++ events.py
@@ -1,8 +1,9 @@
 from abc import  ABC, abstractmethod
-
+import functools
 import graphs
 from utils import *
 
+@functools.total_ordering
 class Event(ABC):
     def __init__(self, time):
         self.uid = str(gensym())
@@ -17,9 +18,9 @@ class Event(ABC):
 
     def __int__(self):
         return int(self.uid)
-
-    def __cmp__(self, other):
-        return int(self) - int(other)
+    
+    def __lt__(self, other): 
+        return self.time < other.time
 
     def __index__(self):
         return int(self)
@@ -28,13 +29,17 @@ class Event(ABC):
 class TransferFinishEvent(Event):
     __hash__ = Event.__hash__
 
-    def __init__(self, time, task):
+    def __init__(self, time, task, machine, data):
         super().__init__(time)
-
+        self.machine_id = graphs.to_id(machine)
         self.task_id = graphs.to_id(task) # Task who requested a fetch
+        self.data = data
 
     def transform_graphs(self, pg: graphs.ProgramGraph, mg: graphs.MachineGraph):
         pg[self.task_id].state = graphs.NodeState.READY
+        # The machine who requested a fetch now has all the data associated 
+        # with this transfer
+        mg[self.machine_id].ready_inputs.update(self.data)
 
 class TaskFinishEvent(Event):
     __hash__ = Event.__hash__
@@ -46,6 +51,7 @@ class TaskFinishEvent(Event):
         self.machine_id = graphs.to_id(machine)
 
     def transform_graphs(self, pg : graphs.ProgramGraph, mg: graphs.MachineGraph):
-        pg[self.machine_id].stored_output.add(self.task_id)
-        mg[self.task_id].state = graphs.NodeState.COMPLETED
+        mg[self.machine_id].stored_output.add(self.task_id)
+        pg[self.task_id].state = graphs.NodeState.COMPLETED
+        mg[self.machine_id].task = None
 
