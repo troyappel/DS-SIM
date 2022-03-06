8f828cf4717875e09ec15125eacd76c3b9638e66
Varun Tekur <v_tekur@yahoo.com>
2022-03-05 22:44:01 -0500

    mapreduce generator

--- generate_programs.py
+++ generate_programs.py
@@ -1,19 +1,27 @@
 import graphs
 
-def generate_tree_program_graph(layers=2, branching=3, compute=4, data=1, affinities=None):
 
-    def machine_affinity(node_type):
-        if node_type == 'machine':
+def generate_type_affinity(type):
+    '''
+        Returns an affinity function ensuring that
+        a task will only be scheduled on nodes with label
+        'type'
+    '''
+    def affinities(x):
+        if x == type:
             return 1
         else:
             return 0
+    return affinities
+
+def generate_tree_program_graph(layers=2, branching=3, compute=4, data=1, affinities=None):
 
     def generate_tree(layers=2, branching=3, compute=4, data=1, affinities=None):
         if layers == 0:
             return None, [], []
 
         if affinities is None:
-            affinities = machine_affinity
+            affinities = generate_type_affinity('machine')
 
         # add coalescing node for this layer
         c_node = graphs.ProgramNode(1,affinities=affinities)
@@ -28,3 +36,43 @@ def generate_tree_program_graph(layers=2, branching=3, compute=4, data=1, affini
 
     _, p_nodes, p_edges = generate_tree(layers, branching, compute, data, affinities)
     return graphs.ProgramGraph(p_nodes, p_edges)
+
+def generate_mapreduce(num_map, num_reduce, map_compute, reduce_compute, map_input_sz, map_output_sz, reduce_output_sz):
+
+    '''
+        Generates a mapreduce computation graph for an arbitrary number of mappers/reducers, compute costs, and 
+        amount of data transferred
+    '''
+
+    dist_func = lambda d: 1/(1+d**2)
+
+    # mapper nodes
+    mapper_disk_input = [graphs.ProgramNode(0, id=f'map_input_{i}', 
+                         affinities=generate_type_affinity('disk'), dist_affinity=dist_func) for i in range(num_map)]
+    mappers = [graphs.ProgramNode(map_compute, id=f'map_{i}', 
+               affinities=generate_type_affinity('machine'), dist_affinity=dist_func) for i in range(num_map)]
+    mapper_disk_output = [graphs.ProgramNode(0, id=f'map_output_{i}', 
+                        affinities=generate_type_affinity('disk'), dist_affinity=dist_func) for i in range(num_map)]
+
+    # reducer nodes
+    reducers = [graphs.ProgramNode(reduce_compute, id=f'reduce_{i}', 
+               affinities=generate_type_affinity('machine'), dist_affinity=dist_func) for i in range(num_reduce)]
+    reducer_disk_output = [graphs.ProgramNode(0, id=f'reduce_output_{i}', 
+                          affinities=generate_type_affinity('disk'), dist_affinity=dist_func) for i in range(num_reduce)]
+
+    # mapper input/output edges
+    mapper_in = [graphs.ProgramEdge(f'map_input_{i}', f'map_{i}', data_size=map_input_sz) for i in range(num_map)]
+    mapper_out = [graphs.ProgramEdge(f'map_{i}', f'map_output_{i}', data_size=map_output_sz) for i in range(num_map)]
+
+    # mapper to reducer edges
+    mapper_to_reducer = [graphs.ProgramEdge(f'map_output_{i}', f'reduce_{j}', map_output_sz) for i in range(num_map) for j in range (num_reduce)]
+
+    # reducer output edges
+    reducer_out = [graphs.ProgramEdge(f'reduce_{i}', f'reduce_output_{i}', data_size=reduce_output_sz) for i in range(num_reduce)]
+
+    p_nodes = mapper_disk_input + mappers + mapper_disk_output + reducers + reducer_disk_output
+    p_edges = mapper_in + mapper_out + mapper_to_reducer + reducer_out
+    return graphs.ProgramGraph(p_nodes, p_edges)
+
+pg = generate_mapreduce(3, 2, 1, 1, 1, 1, 1)
+pg.draw()
